c704745613361cedbf027f803ca694a863c52304
Zakaria Kerkeb <hikkary@MacBook-Pro-de-Zakaria.local>
2025-02-17 15:49:35 +0100

    progress

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import pokemonsList from './data/pokemonsList.js';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import fs from 'fs';
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-// Obtenir le chemin absolu du fichier actuel en utilisant les modules ES
+// Lire le fichier JSON
 const __filename = fileURLToPath(import.meta.url);
-// __filename contient le chemin complet du fichier actuel
-
-// Obtenir le chemin absolu du répertoire actuel
 const __dirname = path.dirname(__filename);
-// __dirname contient le chemin complet du répertoire où se trouve le fichier actuel
+const pokemonsList = JSON.parse(fs.readFileSync(path.join(__dirname, './data/pokemons.json'), 'utf8'));
 
 const app = express();
 const PORT = 3000;
-
-console.log(process.env);
 
 // Middleware pour CORS
 app.use(cors());
@@ -31,14 +26,39 @@ app.use(express.json());
 // '/assets' est le chemin virtuel où les fichiers seront accessibles
 // 'express.static' est un middleware qui sert des fichiers statiques
 // 'path.join(__dirname, '../assets')' construit le chemin absolu vers le dossier 'assets'
-app.use('/assets', express.static(path.join(__dirname, '../assets')));
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
 
 // Route GET de base
-app.get('/api/pokemons', (req, res) => {
-    res.json(pokemonsList);
+app.get("/api/pokemons", (req, res) => {
+  res.status(200).send({
+    types: [
+      "fire",
+      "water",
+      "grass",
+      "electric",
+      "ice",
+      "fighting",
+      "poison",
+      "ground",
+      "flying",
+      "psychic",
+      "bug",
+      "rock",
+      "ghost",
+      "dragon",
+      "dark",
+      "steel",
+      "fairy",
+    ],
+    pokemons: pokemonsList,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("bienvenue sur l'API Pokémon");
 });
 
 // Démarrage du serveur
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
